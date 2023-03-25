26e00efa84056db615ca2be43e804b4ffde2ae63
Thomas Mickley-Doyle <tmickleydoyle@gmail.com>
2023-03-25 11:25:02 -0500

    Remove side of hidden layer

--- pages/api/forecast.js
+++ pages/api/forecast.js
@@ -35,7 +35,7 @@ export default async function handler(req, res) {
     const inputData = body.inputData.split(',').map(x => Number(x.trim()));
     const input = [];
     const windowSize = body.windowSize || 5;
-    const nn = new NeuralNetwork(windowSize, Math.round(windowSize + 1), 1);
+    const nn = new NeuralNetwork(windowSize, Math.round((windowSize + 1) / 2), 1);
 
     const target = inputData.slice(windowSize).map((x) => [x]);
 
