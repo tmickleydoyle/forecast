25035fb061fefdb1b77bc0c2763bffeda2801f3d
Thomas Mickley-Doyle <tmickleydoyle@gmail.com>
2023-04-10 18:17:16 -0500

    Add link to donate to community garden

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -15,6 +15,8 @@ class Footer extends Component {
                 <a href="https://github.com/tmickleydoyle/forecast/issues/new"><span role="img" aria-label="bug emoji">🐛</span> report a bug</a>
                 <a className='greytext'> | </a>
                 <a href="https://tmickleydoyle.vercel.app"><span role="img" aria-label="author">👨🏻‍🎨</span> Thomas Mickley-Doyle</a>
+                <a className='greytext'> | </a>
+                <a href="https://www.sproutnolafarm.org"><span role="img" aria-label="donate">💰</span> donate to my community garden if you find this useful</a>
               </div>
               <br />
           </div>
